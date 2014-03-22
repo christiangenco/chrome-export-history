eba8eb1ab42ff9d2dc0337c2a2248540f1273122
Christian Genco <christian.genco@gmail.com>
2014-03-22 06:31:17 -0500

    consolidating

--- popup.js
+++ popup.js
@@ -1,29 +1,38 @@
-// http://stackoverflow.com/questions/3665115/create-a-file-in-memory-for-user-to-download-not-through-server
-// function download(filename, text) {
-//   var pom = document.createElement('a');
-//   pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-//   pom.setAttribute('download', filename);
-//   pom.click();
-// }
-
-function download(format){
-  alert(format);
+var append = function(text){
+  data.appendChild(document.createTextNode(text));
 }
 
-document.getElementById('json')
-
 document.addEventListener('DOMContentLoaded', function () {
-  // chrome.history.search({
-  //   'text': '', 
-  //   'maxResults': 100, 
-  //   // 'maxResults': 100000, 
-  //   'startTime': 0
-  // }, function(res){
-  //   // console.dir(res);
-  //   download("history.json", JSON.stringify(res));
-  // })
+  window.data = document.getElementById('data');
 
-  // chrome.tabs.create({'url': 'json.html'});
+  document.getElementById('json').onclick = function(){
+    chrome.history.search({
+      'text': '', 
+      // 'maxResults': 100, 
+      'maxResults': 100000, 
+      'startTime': 0
+    }, function(res){
+      window.res = res;
 
+      var text;
+      append("[");
+      for(var i=0; i<res.length; i++){
+        text = JSON.stringify(res[i]);
+        if(i !== res.length-1) text = text + ',';
+        append(text);
+      }
+      append("]");
+
+      window.blob = new Blob([data.innerText], {type: 'application/octet-binary'});
+      window.url = URL.createObjectURL(blob);
+
+      var pom = document.createElement('a');
+      pom.setAttribute('href', url);
+      pom.setAttribute('download', "test.json");
+      pom.click();
+
+      window.close();
+    });
+  };
 });
 
